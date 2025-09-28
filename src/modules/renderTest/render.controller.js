d74ea2682312bd1c320cf690eaab7d7624f13a10
Ahmad Amr <gamerteacher12@gmail.com>
2025-09-28 23:02:06 +0300

    test : render file 7th

--- src/modules/renderTest/render.controller.js
+++ src/modules/renderTest/render.controller.js
@@ -6,7 +6,7 @@ import { testSmtp, verifySmtp } from "./services/render.services.js";
 const renderController = Router();
 
 renderController.get('/smtp/verify', verifySmtp);
-renderController.get('/net/smtp-test/:host/:port', testSmtp);
+renderController.get('/net/smtp-test', testSmtp);
 
 
 
